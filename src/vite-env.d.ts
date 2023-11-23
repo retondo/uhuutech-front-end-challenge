6fbfc18d38a0d1860a69e5bb0614902a5b1e8121
Renan <renan.retondo@gmail.com>
2023-11-23 14:57:58 -0300

    chore(env): add vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_TMDB_V3_BASE_URL: string;
+  readonly VITE_TMDB_V3_JWT_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
