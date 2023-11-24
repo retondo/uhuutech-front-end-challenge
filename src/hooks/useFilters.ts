d23473fb85c83b795382ddf3e1bea56d55d90ecf
Renan <renan.retondo@gmail.com>
2023-11-24 15:52:37 -0300

    refactor(hooks): useFilter use FilterService methods

--- src/hooks/useFilters.ts
+++ src/hooks/useFilters.ts
@@ -1,65 +1,31 @@
 import { useCallback, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
-
-type Filters = {
-  genre: number[];
-};
-
-function parser(searchParams: URLSearchParams): Filters {
-  const searchParamsObj = Object.fromEntries(searchParams.entries());
-  return {
-    genre: searchParamsObj.genre?.split(",").map((id) => Number(id)) ?? [],
-  };
-}
+import FilterService from "@app/services/FilterService";
 
 export default function useFilters() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const filters = useMemo(() => parser(searchParams), [searchParams]);
+  const filters = useMemo(
+    () => FilterService.parse(searchParams),
+    [searchParams],
+  );
 
   const setFilter = useCallback(
     (key: string, value: string | number, append: boolean = false) => {
-      const hasFilter = searchParams.has(key);
-
-      if (hasFilter && append) {
-        return setSearchParams((prevSearchParams) => {
-          const filter = prevSearchParams.get(key)!.split(",");
-          filter.push(String(value));
-          prevSearchParams.set(key, filter.join(","));
-          return prevSearchParams;
-        });
-      }
-
       setSearchParams((prevSearchParams) => {
-        prevSearchParams.set(key, String(value));
-        return prevSearchParams;
+        return FilterService.set(key, value, append, prevSearchParams);
       });
     },
-    [searchParams, setSearchParams],
+    [setSearchParams],
   );
 
   const removeFilter = useCallback(
     (key: string, value?: string | number) => {
-      const hasFilter = searchParams.has(key);
-
-      if (hasFilter) {
-        if (value) {
-          return setSearchParams((prevSearchParams) => {
-            const filter = searchParams.get(key)!.split(",");
-            const index = filter.findIndex((v) => v == value);
-            filter.splice(index, 1);
-            prevSearchParams.set(key, filter.join(","));
-            return prevSearchParams;
-          });
-        }
-
-        setSearchParams((prevSearchParams) => {
-          prevSearchParams.delete(key);
-          return prevSearchParams;
-        });
-      }
+      setSearchParams((prevSearchParams) => {
+        return FilterService.remove(key, value, prevSearchParams);
+      });
     },
-    [searchParams, setSearchParams],
+    [setSearchParams],
   );
 
   return {
