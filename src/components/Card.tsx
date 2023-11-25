810b68a8ca740c416e3af6f9839b9774e91f17ec
Renan <renan.retondo@gmail.com>
2023-11-25 19:01:47 -0300

    feat(components): add to Card image placeholder

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,17 @@
 type CardProps = {
-  imgSrc: string;
+  imgSrc: string | null;
   imgAlt: string;
   title: string;
   description: string;
 };
+
+function ImagePlaceholder() {
+  return (
+    <div className="flex h-[inherit] flex-col place-content-center bg-gray-100">
+      <span className="text-center text-sm text-gray-300">Sem imagem</span>
+    </div>
+  );
+}
 
 export default function Card({
   imgSrc,
@@ -13,11 +21,15 @@ export default function Card({
 }: CardProps) {
   return (
     <div className="col-span-2 flex flex-col gap-2 bg-white">
-      <img
-        className="rounded border border-[#e7e7e7]"
-        src={imgSrc}
-        alt={imgAlt}
-      />
+      {imgSrc ? (
+        <div className="overflow-hidden rounded border border-[#e7e7e7]">
+          <img src={imgSrc} alt={imgAlt} />
+        </div>
+      ) : (
+        <div className="h-full overflow-hidden rounded border border-[#e7e7e7]">
+          <ImagePlaceholder />
+        </div>
+      )}
       <div>
         <p className="text-base font-bold">{title}</p>
         <p className="text-sm font-bold uppercase text-[#646464]">
