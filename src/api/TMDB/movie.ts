be0d98decc8c923ae63c1473352020bde088c293
Renan <renan.retondo@gmail.com>
2023-11-26 16:57:22 -0300

    refactor(api): remove date format from movie mapper

--- src/api/TMDB/movie.ts
+++ src/api/TMDB/movie.ts
@@ -4,21 +4,10 @@ import type { Movie, QueryParams, RequestConfig, ResponseList } from "./types";
 const path = "/movie";
 const imgBaseURL = "https://image.tmdb.org/t/p/original";
 
-function dateFormatter(date: string): string {
-  const d = new Date(date);
-
-  const day = d.toLocaleString("default", { day: "2-digit" });
-  const month = d.toLocaleString("default", { month: "short" });
-  const year = d.toLocaleString("default", { year: "numeric" });
-
-  return `${day} ${month} ${year}`;
-}
-
 export function mapper(movies: Movie[]): Movie[] {
   return movies.map((movie) => ({
     ...movie,
     poster_url: movie.poster_path ? `${imgBaseURL}${movie.poster_path}` : null,
-    release_date: dateFormatter(movie.release_date),
   }));
 }
 
