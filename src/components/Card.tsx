3586c55d7196a230d25f257bdfa4f2a8ad8a4b57
Renan <renan.retondo@gmail.com>
2023-11-26 17:01:18 -0300

    refactor(components): use Image in Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,11 @@
+import Image from "./Image";
+
 type CardProps = {
   imgSrc: string | null;
   imgAlt: string;
   title: string;
   description: string;
 };
-
-function ImagePlaceholder() {
-  return (
-    <div className="flex h-[inherit] flex-col place-content-center bg-gray-100">
-      <span className="text-center text-sm text-gray-300">Sem imagem</span>
-    </div>
-  );
-}
 
 export default function Card({
   imgSrc,
@@ -20,16 +14,10 @@ export default function Card({
   description,
 }: CardProps) {
   return (
-    <div className="col-span-2 flex flex-col gap-2 bg-white">
-      {imgSrc ? (
-        <div className="overflow-hidden rounded border border-[#e7e7e7]">
-          <img src={imgSrc} alt={imgAlt} />
-        </div>
-      ) : (
-        <div className="h-full overflow-hidden rounded border border-[#e7e7e7]">
-          <ImagePlaceholder />
-        </div>
-      )}
+    <div className="flex flex-col gap-2 bg-white">
+      <div className="min-h-[264px] overflow-hidden rounded border border-[#e7e7e7]">
+        <Image src={imgSrc} alt={imgAlt} />
+      </div>
       <div>
         <p className="text-base font-bold">{title}</p>
         <p className="text-sm font-bold uppercase text-[#646464]">
