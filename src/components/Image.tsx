2c7a2c547fffd6445f554e219a8e0519498e4bfd
Renan <renan.retondo@gmail.com>
2023-11-26 17:00:42 -0300

    feat(components): add Image

---
+++ src/components/Image.tsx
@@ -0,0 +1,16 @@
+type ImageProps = {
+  src: string | null;
+  alt: string;
+};
+
+function ImagePlaceholder() {
+  return (
+    <div className="flex h-[inherit] min-h-[inherit] flex-col place-content-center bg-gray-100">
+      <span className="text-center text-sm text-gray-300">Sem imagem</span>
+    </div>
+  );
+}
+
+export default function Image({ src, alt }: ImageProps) {
+  return src ? <img src={src} alt={alt} /> : <ImagePlaceholder />;
+}
