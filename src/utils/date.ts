b228fbf3b15ef4cc7223eaa37258647ca928c8e0
Renan <renan.retondo@gmail.com>
2023-11-27 12:35:28 -0300

    feat(utils): add default values to options of dateFormatter

---
+++ src/utils/date.ts
@@ -0,0 +1,28 @@
+type DateFormatterOptions = Partial<
+  {
+    separator: string;
+  } & Pick<Intl.DateTimeFormatOptions, "day" | "month" | "year">
+>;
+
+const defaultDateFormatterOptions: DateFormatterOptions = {
+  day: "2-digit",
+  month: "numeric",
+  year: "numeric",
+  separator: "/",
+};
+
+export default function dateFormatter(
+  date: string,
+  options: DateFormatterOptions = {},
+): string {
+  const d = new Date(date);
+  const mergedOptions = { ...defaultDateFormatterOptions, ...options };
+
+  const day = d.toLocaleString("default", { day: mergedOptions.day });
+  const month = d.toLocaleString("default", { month: mergedOptions.month });
+  const year = d.toLocaleString("default", { year: mergedOptions.year });
+
+  const { separator } = mergedOptions;
+
+  return [day, month, year].join(separator);
+}
