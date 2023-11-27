23ab175f8081b3944e23b148e99a94039a8e6ff4
Renan <renan.retondo@gmail.com>
2023-11-27 13:24:16 -0300

    feat(components): add height inherit to Image

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -5,7 +5,7 @@ type ImageProps = {
 
 function ImagePlaceholder() {
   return (
-    <div className="flex h-[inherit] min-h-[inherit] flex-col place-content-center bg-gray-100">
+    <div className="flex h-full min-h-[inherit] flex-col place-content-center bg-gray-100">
       <span className="text-center text-sm text-gray-300">Sem imagem</span>
     </div>
   );
@@ -13,7 +13,11 @@ function ImagePlaceholder() {
 
 export default function Image({ src, alt }: ImageProps) {
   return src ? (
-    <img className="h-[inherit] min-h-[inherit]" src={src} alt={alt} />
+    <img
+      className="h-[inherit] max-h-[inherit] min-h-[inherit]"
+      src={src}
+      alt={alt}
+    />
   ) : (
     <ImagePlaceholder />
   );
