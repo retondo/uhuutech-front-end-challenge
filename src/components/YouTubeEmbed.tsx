32ff64f17e88ed140ecc84ae4a71e45adc6eecea
Renan <renan.retondo@gmail.com>
2023-11-28 15:43:31 -0300

    feat(components): add allowFullScreen attribute to YouTubeEmbed

--- src/components/YouTubeEmbed.tsx
+++ src/components/YouTubeEmbed.tsx
@@ -7,6 +7,7 @@ export default function YouTubeEmbed({ srcKey }: YouTubeEmbedProps) {
     <iframe
       className="max-h-[182px] min-h-[182px] min-w-[324px] max-w-[324px] md:max-h-[243px] md:min-h-[243px] md:min-w-[433px] md:max-w-[433px] lg:max-h-[510px] lg:min-h-[510px] lg:min-w-[907px] lg:max-w-[907px]"
       src={`https://www.youtube.com/embed/${srcKey}`}
+      allowFullScreen
     ></iframe>
   ) : null;
 }
